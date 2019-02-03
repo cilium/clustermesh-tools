7e9e2b7f247e10af6a9dce6c6a12a9d1cafe2daa
Thomas Graf <thomas@cilium.io>
2019-02-03 23:29:04 +0100

    Generate a kubectl patch to edit hostAliases
    Signed-off-by: Thomas Graf <thomas@cilium.io>

--- generate-name-mapping.sh
+++ generate-name-mapping.sh
@@ -4,12 +4,17 @@ set -e
 
 DIR="${OUTPUT:-config}"
 
-echo "hostAliases:"
+cat << EOF
+spec:
+  template:
+    spec:
+      hostAliases:
+EOF
 
 for IPFILE in $DIR/*.ips; do
 	for IP in $(cat $IPFILE); do
-		echo "- ip: \"$IP\""
-		echo "  hostnames:"
-		echo "  - $(basename $IPFILE | sed -e s/.ips$//)"
+		echo "      - ip: \"$IP\""
+		echo "        hostnames:"
+		echo "        - $(basename $IPFILE | sed -e s/.ips$//)"
 	done
 done
