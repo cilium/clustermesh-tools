8b78d3425d895c6deb7dcffcb3613aaec3e91fff
Bruno Miguel Custódio <brunomcustodio@gmail.com>
2019-12-09 08:52:00 +0000

    Support services of type 'NodePort'.
    Signed-off-by: Bruno Miguel Custódio <brunomcustodio@gmail.com>

--- extract-etcd-secrets.sh
+++ extract-etcd-secrets.sh
@@ -3,6 +3,7 @@
 set -e
 
 NAMESPACE="${NAMESPACE:-kube-system}"
+SERVICE_NAME="${SERVICE_NAME:-cilium-etcd-external}"
 DIR="${OUTPUT:-config}"
 
 if [ -z "$CLUSTER_NAME" ]; then
@@ -25,26 +26,47 @@ for SECRET in $SECRETS; do
   echo "$SECRET" | jq -r '.value' | base64 --decode > "$DIR/$CLUSTER_NAME.$KEY"
 done
 
+SERVICE=$(kubectl -n "$NAMESPACE" get svc "$SERVICE_NAME" -o json)
+SERVICE_TYPE=$(echo "$SERVICE" | jq -r -c '.spec.type')
 
-IPS=$(kubectl -n "$NAMESPACE" get svc cilium-etcd-external -o json | jq -r -c '.status.loadBalancer.ingress[0].ip')
-if [ -z "$IPS" ] || [ "$IPS" = null ]; then
-	HOSTNAME=$(kubectl -n "$NAMESPACE" get svc cilium-etcd-external -o json | jq -r -c '.status.loadBalancer.ingress[0].hostname')
-	if [ -z "$HOSTNAME" ] || [ "$HOSTNAME" == null ]; then
-		echo "Unable to determine hostname for service cilium-etcd-external. .status.loadBalancer.ingress[0].hostname is empty"
-		exit 1
+case "$SERVICE_TYPE" in
+"NodePort")
+	# Grab the node's internal IPs.
+	IPS=$(kubectl -n "$NAMESPACE" get node \
+		-o jsonpath='{.items[*].status.addresses[?(@.type=="InternalIP")].address}' | tr ' ' '\n')
+	# Grab the node port on which etcd is exposed.
+	PORT=$(kubectl -n "$NAMESPACE" get svc "$SERVICE_NAME" \
+		-o jsonpath='{.spec.ports[?(@.port==2379)].nodePort}')
+	;;
+"LoadBalancer")
+	# Grab the load-balancer's IP(s).
+	IPS=$(echo "$SERVICE" | jq -r -c '.status.loadBalancer.ingress[0].ip')
+	if [ -z "$IPS" ] || [ "$IPS" = null ]; then
+		HOSTNAME=$(echo "$SERVICE" | jq -r -c '.status.loadBalancer.ingress[0].hostname')
+		if [ -z "$HOSTNAME" ] || [ "$HOSTNAME" == null ]; then
+			echo "Unable to determine hostname for service $SERVICE_NAME. .status.loadBalancer.ingress[0].hostname is empty"
+			exit 1
+		fi
+		IPS=$(host "$HOSTNAME" | grep address | awk '{print $NF}')
+		if [ -z "$IPS" ]; then
+			echo "Unable to resolve hostname $HOSTNAME to IP"
+			exit 1
+		fi
 	fi
-	IPS=$(host "$HOSTNAME" | grep address | awk '{print $NF}')
-	if [ -z "$IPS" ]; then
-		echo "Unable to resolve hostname $HOSTNAME to IP"
-		exit 1
-	fi
-fi
+	# Use '2379' as the port, as that's what load-balancers will be using.
+	PORT="2379"
+	;;
+*)
+	echo "Services of type $SERVICE_TYPE are not supported. Please use NodePort or LoadBalancer."
+	exit 1
+	;;
+esac
 
 SERVICE_NAME="${CLUSTER_NAME}.mesh.cilium.io"
 
 cat > "$DIR/$CLUSTER_NAME" << EOF
 endpoints:
-- https://${SERVICE_NAME}:2379
+- https://${SERVICE_NAME}:${PORT}
 EOF
 
 echo "$IPS"  > "$DIR/${SERVICE_NAME}.ips"
